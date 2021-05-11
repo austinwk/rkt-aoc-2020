0f19729d20e6d08c6a48e16dc526837783da47ed
austin <austinwkeeton@protonmail.com>
2021-05-11 07:00:34 -0500

    Finish day 3.

--- 03.rkt
+++ 03.rkt
@@ -4,7 +4,13 @@
 
 ;;------------------------------------------------------------------------------
 ;; Day 3
+;;
+;; `in-lines` could be used in place of `in-list`, but that would require
+;; closing the port after the procedure ends. `in-list` is simpler.
 ;;------------------------------------------------------------------------------
+
+(define (get-input)
+  (file->lines "03.txt"))
 
 ;;------------------------------------------------------------------------------
 ;; Part 1
@@ -13,7 +19,7 @@
 (define (solve-part-1)
   (for/fold ([trees 0])
             ([row (in-range +inf.0)]
-             [line (in-list (file->lines "03.txt"))])
+             [line (in-list (get-input))])
     (if (char=? #\# (string-ref line (modulo (* row 3) 31)))
         (add1 trees)
         trees)))
@@ -22,8 +28,21 @@
 ;; Part 2
 ;;------------------------------------------------------------------------------
 
+(define (count-trees right down)
+  (for/fold ([col 0]
+             [trees 0]
+             #:result trees)
+            ([(line row) (in-indexed (get-input))]
+             #:when (zero? (modulo row down)))
+    (values (+ col right)
+            (if (char=? #\# (string-ref line (modulo col 31)))
+                (add1 trees)
+                trees))))
+
 (define (solve-part-2)
-  (error "unimplemented"))
+  (for/fold ([product 1])
+            ([slope (in-list '((1 1) (3 1) (5 1) (7 1) (1 2)))])
+    (* product (apply count-trees slope))))
 
 ;;------------------------------------------------------------------------------
 ;; Tests
@@ -33,5 +52,4 @@
   (require rackunit)
 
   (test-eq? "solve-part-1" (solve-part-1) 276)
-  ; (test-eq? "solve-part-2" (solve-part-2) #f)
-)
+  (test-eq? "solve-part-2" (solve-part-2) 7812180000))
