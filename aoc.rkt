c9eb0bf1507fa7b0d20e383358894a3dd33273df
austin <austinwkeeton@protonmail.com>
2021-05-12 07:13:21 -0500

    aoc script

--- aoc.rkt
+++ aoc.rkt
@@ -3,16 +3,26 @@
 (require net/http-client)
 
 (define (get-session-id)
-  (with-input-from-file ".session-id"
-                        (lambda ()
-                          (read-line))))
+  (call-with-input-file ".session-id"
+                             (lambda (port)
+                               (read-line port))))
 
-(define (get-input)
+(define (get-input day)
   (let-values ([(status headers in)
                 (http-sendrecv "adventofcode.com"
-                               "/2020/day/4/input"
+                               (format "/2020/day/~a/input" day)
                                #:ssl? #t
                                #:headers (list (format "Cookie:	session=~a" (get-session-id))))])
-    (displayln (port->string in))))
+    (port->string in)))
 
-(displayln (get-input))
+(define (write-input day data)
+  (call-with-output-file (string-append (~a day
+                                            #:min-width 2
+                                            #:align 'right
+                                            #:left-pad-string "0")
+                                        ".txt")
+                           (lambda (port) (display data port))))
+
+(module+ main
+  (let ([day (vector-ref (current-command-line-arguments) 0)])
+  (write-input day (get-input day))))
