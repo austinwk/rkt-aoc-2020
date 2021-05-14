04d5f538fb85c3dfd39c64ed0b5e03b9ceb61910
austin <austinwkeeton@protonmail.com>
2021-05-14 07:29:45 -0500

    Finish 4-1, start 4-2.

--- 04.rkt
+++ 04.rkt
@@ -28,22 +28,66 @@
                                       (pp-field k v)))
                                   batch))])))))
 
-(displayln (get-passports))
-      
-
 ;;------------------------------------------------------------------------------
 ;; Part 1
 ;;------------------------------------------------------------------------------
 
+; Get a line
+; Dice it up
+; Gather a batch
+; Batch valid?
+
 (define (solve-part-1)
-  (error "unimplemented"))
+  (call-with-input-file
+    "04.txt"
+    (lambda (in)
+      (let iter ([line (read-line in)]
+                 [valid 0]
+                 [batch '()])
+        (cond [(eof-object? line) valid]
+              [(string=? "" line) (if (for/and ([field '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid")])
+                                        (member field batch))
+                                      (iter (read-line in) (add1 valid) '())
+                                      (iter (read-line in) valid '()))]
+              [else (iter (read-line in)
+                          valid
+                          (append batch
+                                  (for/list ([token (string-split line)])
+                                    (substring token 0 3))))])))))
 
 ;;------------------------------------------------------------------------------
 ;; Part 2
 ;;------------------------------------------------------------------------------
 
+(define fields #hash(("byr" . (lambda (v) (string>=? "1920" v "2002")))
+                     ("iyr" . (lambda (v) (string>=? "2010" v "2020")))
+                     ("eyr" . (lambda (v) (string>=? "2020" v "2030")))
+                     ("hgt" . (lambda (v) (let* ([len (string-length v)]
+                                                 [num (substring v 0 (- len 2))]
+                                                 [unt (substring v (- len 2))])
+                                            (case unt
+                                              [("cm") (string>=? "150" num "193")]
+                                              [("in") (string>=? "59" num "76")]
+                                              [else #f]))))))
+
 (define (solve-part-2)
-  (error "unimplemented"))
+  (call-with-input-file
+    "04.txt"
+    (lambda (in)
+      (let iter ([line (read-line in)]
+                 [valid 0]
+                 [batch '()])
+        (cond [(eof-object? line) valid]
+              [(string=? "" line) (if (for/and ([field '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid")])
+                                        (let ([kv (assoc field batch)])
+                                          (and kv (case ))))
+                                      (iter (read-line in) (add1 valid) '())
+                                      (iter (read-line in) valid '()))]
+              [else (iter (read-line in)
+                          valid
+                          (append batch
+                                  (for/list ([token (string-split line)])
+                                    (string-split token ":"))))])))))
 
 ;;------------------------------------------------------------------------------
 ;; Tests
@@ -52,5 +96,5 @@
 (module+ test
   (require rackunit)
 
-  (test-eq? "solve-part-1" (solve-part-1) #f)
-  (test-eq? "solve-part-2" (solve-part-2) #f))
+  (test-eq? "solve-part-1" (solve-part-1) 202)
+  (test-eq? "solve-part-2" (solve-part-2) 137))
