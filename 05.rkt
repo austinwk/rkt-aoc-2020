a13e78f426ec8a8c56092f7d8595178bd06a0149
Austin W Keeton <austinwkeeton@protonmail.com>
2021-05-16 10:06:28 -0500

    Finish 5-1.

--- 05.rkt
+++ 05.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 ;;------------------------------------------------------------------------------
-;; Day #
+;; Day 5
 ;;------------------------------------------------------------------------------
 
 (define (get-input)
@@ -12,7 +12,34 @@
 ;;------------------------------------------------------------------------------
 
 (define (solve-part-1)
-  (error "unimplemented"))
+  (call-with-input-file
+    "05.txt"
+    (lambda (in)
+      (for/fold ([highest-seat-id 0])
+                ([pass (in-port read-line in)])
+        (let ([id (seat-id pass)])
+          (if (> id highest-seat-id)
+              id
+              highest-seat-id)))))) ;=> 855
+
+(define (seat-id pass)
+  (+ (* 8 (seat-row pass)) (seat-col pass)))
+
+(define (seat-row pass)
+  (for/fold ([seats (range 128)]
+             #:result (first seats))
+            ([chr (in-list (string->list (substring pass 0 6)))])
+    (if (char=? chr #\F)
+        (take seats (/ (length seats) 2))
+        (drop seats (/ (length seats) 2)))))
+
+(define (seat-col pass)
+  (for/fold ([seats (range 8)]
+             #:result (first seats))
+            ([chr (in-list (string->list (substring pass 7)))])
+    (if (char=? chr #\L)
+        (take seats (/ (length seats) 2))
+        (drop seats (/ (length seats) 2)))))
 
 ;;------------------------------------------------------------------------------
 ;; Part 2
