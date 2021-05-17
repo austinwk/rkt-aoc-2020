c3495ae1d160b55bfef35df65179c62eab0dad09
austin <austinwkeeton@protonmail.com>
2021-05-17 06:18:49 -0500

    Solve 5-2.

--- 05.rkt
+++ 05.rkt
@@ -4,6 +4,23 @@
 ;; Day 5
 ;;------------------------------------------------------------------------------
 
+(define (seat-id pass)
+  (+ (* 8 (seat-row pass)) (seat-col pass)))
+
+(define (seat-row pass)
+  (search-seats (substring pass 0 7) 128 #\F))
+
+(define (seat-col pass)
+  (search-seats (substring pass 7) 8 #\L))
+
+(define (search-seats instructions num-seats lower-char)
+  (for/fold ([seats (range num-seats)]
+             #:result (first seats))
+            ([char (in-list (string->list instructions))])
+    (if (char=? char lower-char)
+        (take seats (/ (length seats) 2))
+        (drop seats (/ (length seats) 2)))))
+
 ;;------------------------------------------------------------------------------
 ;; Part 1
 ;;------------------------------------------------------------------------------
@@ -19,41 +36,22 @@
               id
               highest-seat-id)))))) ;=> 855
 
-(define (seat-id pass)
-  (+ (* 8 (seat-row pass)) (seat-col pass)))
-
-(define (seat-row pass)
-  (search-seats (substring pass 0 6) 128 #\F))
-
-(define (seat-col pass)
-  (search-seats (substring pass 7) 8 #\L))
-
-(define (search-seats instructions num-seats lower-char)
-  (for/fold ([seats (range num-seats)]
-             #:result (first seats))
-            ([char (in-list (string->list instructions))])
-    (if (char=? char lower-char)
-        (take seats (/ (length seats) 2))
-        (drop seats (/ (length seats) 2)))))
-
 ;;------------------------------------------------------------------------------
 ;; Part 2
 ;;------------------------------------------------------------------------------
 
-; (define (solve-part-2)
-;  (call-with-input-file
-;    "05.txt"
-;    (lambda (in)
-;      (for/fold ([passes '()]
-;                 #:result (sort passes <))
-;                ([pass (in-port read-line in)])
-;        (cons (seat-id pass) passes)))))
-
 (define (solve-part-2)
+  (let ([pass-ids (all-pass-ids)])
+    (for/first ([a (in-list pass-ids)]
+                [b (in-list (cdr pass-ids))]
+                #:when (not (= (add1 a) b)))
+      (add1 a)))) ;=> 552
+
+(define (all-pass-ids)
  (call-with-input-file
    "05.txt"
    (lambda (in)
-     (for/list ([pass (in-port read-line in)])
-       (seat-id pass)))))
-
-; (define (my-seat seat-ids))
+     (for/fold ([passes '()]
+                #:result (sort passes <))
+               ([pass (in-port read-line in)])
+       (cons (seat-id pass) passes)))))
