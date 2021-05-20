726c3a67e18e125075679e5e4029cb39eb7aa820
austin <austinwkeeton@protonmail.com>
2021-05-20 06:46:50 -0500

    Solve 7-1.

--- 07.rkt
+++ 07.rkt
@@ -4,13 +4,13 @@
 ;; Day #
 ;;------------------------------------------------------------------------------
 
-(define (get-input)
+(define (get-rules)
   (call-with-input-file
     "07.txt"
     (lambda (in)
       (define rules (make-hash))
       (for ([line (in-port read-line in)])
-        (let*-values ([(tokens) (string-split line #rx" bags contain no other bags.| bags contain | bags, | bag, | bags.")]
+        (let*-values ([(tokens) (string-split line #rx" bags contain no other bags.| bags contain | bags, | bag, | bags.| bag.")]
                       [(k v) (parse-rule tokens)])
           (hash-set! rules k v)))
       rules)))
@@ -26,7 +26,28 @@
 ;;------------------------------------------------------------------------------
 
 (define (solve-part-1)
-  (error "unimplemented"))
+  (let ([rules (get-rules)])
+    (for/fold ([count 0])
+              ([bag (in-hash-keys rules)])
+      (if (holds-gold? rules bag)
+          (add1 count)
+          count)))) ;=> 274
+
+; Example: (hash "light green" #t)
+(define holds-gold-cache (make-hash))
+
+(define (holds-gold? rules bag)
+  (if (hash-has-key? holds-gold-cache bag)
+      (hash-ref holds-gold-cache bag)
+      (let ([sub-bags (hash-ref rules bag)])
+           (cond [(null? sub-bags)
+                    (hash-set! holds-gold-cache bag #f)
+                    #f]
+                 [(assoc "shiny gold" sub-bags)
+                    (hash-set! holds-gold-cache bag #t)
+                    #t]
+                 [else (for/or ([sub-bag (in-list sub-bags)])
+                         (holds-gold? rules (car sub-bag)))]))))
 
 ;;------------------------------------------------------------------------------
 ;; Part 2
